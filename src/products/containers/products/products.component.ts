bf8b68062e406054dcccd52c769fff8836595096
Sandi Barr <sandi.barr@prx.org>
2017-12-06 11:39:56 -0600

    state composition with selectors

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromStore from '../../store';
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
 
 @Component({
   selector: 'products',
@@ -16,25 +17,23 @@ import { PizzasService } from '../../services/pizzas.service';
         </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
+        <div *ngIf="!((pizzas$ | async)?.length)">
           No pizzas, add one to get started.
         </div>
         <pizza-item
-          *ngFor="let pizza of (pizzas)"
+          *ngFor="let pizza of (pizzas$ | async)"
           [pizza]="pizza">
         </pizza-item>
       </div>
     </div>
-  `,
+  `
 })
 export class ProductsComponent implements OnInit {
-  pizzas: Pizza[];
+  pizzas$: Observable<Pizza[]>;
 
-  constructor(private pizzaService: PizzasService) {}
+  constructor(private store: Store<fromStore.ProductsState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
-    });
+    this.pizzas$ = this.store.select<any>(fromStore.getAllPizzas);
   }
 }
--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -7,17 +7,17 @@ export const LOAD_PIZZAS_FAILURE = '[Products] Load Pizzas Failure';
 export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
 
 export class LoadPizzas implements Action {
-    readonly type = LOAD_PIZZAS;
+  readonly type = LOAD_PIZZAS;
 }
 
 export class LoadPizzasFailure implements Action {
-    readonly type = LOAD_PIZZAS_FAILURE;
-    constructor(public payload: any) {}
+  readonly type = LOAD_PIZZAS_FAILURE;
+  constructor(public payload: any) {}
 }
 
 export class LoadPizzasSuccess implements Action {
-    readonly type = LOAD_PIZZAS_SUCCESS;
-    constructor(public payload: Pizza[]) {}
+  readonly type = LOAD_PIZZAS_SUCCESS;
+  constructor(public payload: Pizza[]) {}
 }
 
 // action types
