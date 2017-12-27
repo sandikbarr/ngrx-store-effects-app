fdebd31d3138b27f0b9b792c0a2f5b1953ddff6b
Sandi Barr <sandi.barr@prx.org>
2017-12-27 14:08:54 -0600

    pizza exists and toppings guards

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -29,11 +29,12 @@ export const ROUTES: Routes = [
   },
   {
     path: 'new',
-    canActivate: [fromGuards.PizzasGuard],
+    canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard],
     component: fromContainers.ProductItemComponent
   },
   {
     path: ':pizzaId',
+    canActivate: [fromGuards.PizzaExistsGuard, fromGuards.ToppingsGuard],
     component: fromContainers.ProductItemComponent
   }
 ];
